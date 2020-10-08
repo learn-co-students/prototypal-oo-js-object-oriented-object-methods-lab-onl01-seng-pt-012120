2bcb72426e8e0cd6e7edc3df79be6afa9bc9979e
Charlie Pelton <charliejp0311@gmail.com>
2020-10-08 12:05:21 -0500

    Done.

--- .history/index_20201008120428.js
+++ .history/index_20201008120428.js
@@ -13,5 +13,5 @@ class BoardMember{
 
     releasePressStatement(){return 'You will see great things from Scuber.'}
 
-    sayHi(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`}
+    sayHi(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`}
 }
